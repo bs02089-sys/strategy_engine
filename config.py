df5c7914c94226d15383671409811dc962f5681b
bs02089-sys <bs02089@gmail.com>
2026-05-11 11:55:25 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+KEYWORDS = [
+    "AI Infrastructure",
+    "semiconductor stock",
+    "NVDA",
+    "SOXL ETF",
+    "TSLA stock",
+    "IONQ stock",
+]
+SCHEDULE_TIMES = ["09:00", "19:00"]
+MAX_NEWS_PER_KEYWORD = 5
